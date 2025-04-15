9ea92b58bf3bc4af781e8d7a2a1676979f5a3f12
tarekahmed220 <tarek.ahm12@gmail.com>
2025-04-15 11:41:18 +0200

    fix bug

--- src/modules/messages/message.routes.js
+++ src/modules/messages/message.routes.js
@@ -2,10 +2,16 @@ import express from "express";
 import { validation } from "../../middleware/validation.js";
 import messageValidation from "./messages.validation.js";
 import { getAllMessages, sendMessage } from "./message.controller.js";
+import { protect, restrictTo } from "../../middleware/authMiddleware.js";
 
 const messagesRoutes = express.Router();
 
-messagesRoutes.get("/get-all-messages", getAllMessages);
+messagesRoutes.get(
+  "/get-all-messages",
+  protect,
+  restrictTo("admin"),
+  getAllMessages
+);
 messagesRoutes.post(
   "/send-message",
   validation(messageValidation),
