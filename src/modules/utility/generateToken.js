2baee262970b164170eabd38018bc03fc6bb8670
tarekahmed220 <tarek.ahm12@gmail.com>
2025-04-22 20:39:23 +0200

    fix domain cookies

--- src/modules/utility/generateToken.js
+++ src/modules/utility/generateToken.js
@@ -28,7 +28,7 @@ const generateToken = (req, res, userId) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "None",
+    sameSite: "lax",
     maxAge: 3 * 24 * 60 * 60 * 1000,
     domain:
       new URL(req.header("referer")).host === "dashboard.tendersday.com"
