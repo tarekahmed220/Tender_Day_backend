0abab0e7d31ba50b0da2d4bd433bfaac4aa88022
tarekahmed220 <tarek.ahm12@gmail.com>
2025-04-22 20:24:11 +0200

    fix domain cookies

--- src/modules/utility/generateToken.js
+++ src/modules/utility/generateToken.js
@@ -16,26 +16,3 @@ const generateToken = (res, userId) => {
 };
 
 export default generateToken;
-
-// import jwt from "jsonwebtoken";
-
-// const generateToken = (req, res, userId) => {
-//   const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-//     expiresIn: "3d",
-//   });
-
-//   res.cookie("token", token, {
-//     httpOnly: true,
-//     secure: process.env.NODE_ENV === "production",
-//     sameSite: "None",
-//     maxAge: 3 * 24 * 60 * 60 * 1000,
-//     domain:
-//       req.hostname === "dashboard.tendersday.com"
-//         ? "dashboard.tendersday.com"
-//         : "tendersday.com",
-//   });
-
-//   return token;
-// };
-
-// export default generateToken;
