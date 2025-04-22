9d49b67625addf27682fd52936e82c27ef1a4664
tarekahmed220 <tarek.ahm12@gmail.com>
2025-04-22 21:37:03 +0200

    fix domain cookies

--- src/modules/utility/generateToken.js
+++ src/modules/utility/generateToken.js
@@ -1,40 +1,15 @@
-// import jwt from "jsonwebtoken";
-
-// const generateToken = (res, userId) => {
-//   const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-//     expiresIn: "3d",
-//   });
-
-//   res.cookie("token", token, {
-//     httpOnly: true,
-//     secure: process.env.NODE_ENV === "production",
-//     sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
-//     maxAge: 3 * 24 * 60 * 60 * 1000,
-//   });
-
-//   return token;
-// };
-
-// export default generateToken;
-
 import jwt from "jsonwebtoken";
 
-const generateToken = (req, res, userId) => {
+const generateToken = (res, userId) => {
   const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, {
     expiresIn: "3d",
   });
-  console.log("req", new URL(req.header("referer")).host);
 
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
     maxAge: 3 * 24 * 60 * 60 * 1000,
-    domain: "tendersday.com",
-    path:
-      new URL(req.header("referer")).host === "dashboard.tendersday.com"
-        ? "/dashboard"
-        : "/",
   });
 
   return token;
