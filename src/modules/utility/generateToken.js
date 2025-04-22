16c656c916b4aec76e2b4659656e02ed0d0bc7e2
tarekahmed220 <tarek.ahm12@gmail.com>
2025-04-22 20:29:41 +0200

    fix domain cookies

--- src/modules/utility/generateToken.js
+++ src/modules/utility/generateToken.js
@@ -1,15 +1,39 @@
+// import jwt from "jsonwebtoken";
+
+// const generateToken = (res, userId) => {
+//   const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, {
+//     expiresIn: "3d",
+//   });
+
+//   res.cookie("token", token, {
+//     httpOnly: true,
+//     secure: process.env.NODE_ENV === "production",
+//     sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+//     maxAge: 3 * 24 * 60 * 60 * 1000,
+//   });
+
+//   return token;
+// };
+
+// export default generateToken;
+
 import jwt from "jsonwebtoken";
 
-const generateToken = (res, userId) => {
+const generateToken = (req, res, userId) => {
   const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, {
     expiresIn: "3d",
   });
+  console.log("req", req.hostname);
 
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+    sameSite: "None",
     maxAge: 3 * 24 * 60 * 60 * 1000,
+    domain:
+      req.hostname === "dashboard.tendersday.com"
+        ? "dashboard.tendersday.com"
+        : "tendersday.com",
   });
 
   return token;
