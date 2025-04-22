be0fa5689f8c2719d921578bc31775ca7badb52d
tarekahmed220 <tarek.ahm12@gmail.com>
2025-04-22 20:17:48 +0200

    fix domain cookies

--- src/modules/utility/generateToken.js
+++ src/modules/utility/generateToken.js
@@ -1,3 +1,22 @@
+import jwt from "jsonwebtoken";
+
+const generateToken = (res, userId) => {
+  const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, {
+    expiresIn: "3d",
+  });
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+    maxAge: 3 * 24 * 60 * 60 * 1000,
+  });
+
+  return token;
+};
+
+export default generateToken;
+
 // import jwt from "jsonwebtoken";
 
 // const generateToken = (req, res, userId) => {
@@ -20,35 +39,3 @@
 // };
 
 // export default generateToken;
-import jwt from "jsonwebtoken";
-
-const generateToken = (req, res, userId) => {
-  const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-    expiresIn: "3d",
-  });
-
-  // تحديد الدومين بناءً على الموقع
-  let cookieDomain;
-  if (req.hostname === "dashboard.tendersday.com") {
-    cookieDomain = "dashboard.tendersday.com";
-  } else if (
-    req.hostname === "tendersday.com" ||
-    req.hostname === "www.tendersday.com"
-  ) {
-    cookieDomain = ".tendersday.com"; // لتغطية tendersday.com و www.tendersday.com
-  } else {
-    cookieDomain = req.hostname; // لأي دومين آخر (اختياري)
-  }
-
-  res.cookie("token", token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // استخدام Lax في التطوير
-    maxAge: 3 * 24 * 60 * 60 * 1000,
-    domain: cookieDomain,
-  });
-
-  return token;
-};
-
-export default generateToken;
