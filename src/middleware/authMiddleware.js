452266168f0820475a14cd7cd9c2b6fdfd8f6f6a
tarekahmed220 <tarek.ahm12@gmail.com>
2025-04-22 21:39:51 +0200

    fix domain cookies

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -7,6 +7,10 @@ import isSubscriptionValid from "../modules/utility/isSubscriptionValid.js";
 export const protect = async (req, res, next) => {
   let token = req.cookies?.token;
 
+  if (!token && req.headers.authorization?.startsWith("Bearer ")) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+
   if (!token) {
     return next(new AppError("من فضلك قم بتسجيل الدخول أولا", 401));
   }
