51806dfd7ca0ef4c313f17b6705f855d1c7018d7
tarekahmed220 <tarek.ahm12@gmail.com>
2025-05-06 15:55:05 +0300

    fix delete users

--- src/modules/fields/fields.controller.js
+++ src/modules/fields/fields.controller.js
@@ -59,7 +59,8 @@ export const getFieldById = catchError(async (req, res, next) => {
     return next(new AppError("المجال غير موجود", 404));
   }
   const page = Number(req.query.page) || 1;
-  const limit = Number(req.query.limit) ?? 10;
+  const limit =
+    Number(req.query.limit) === 0 ? Infinity : Number(req.query.limit) || 10;
   const skip = (page - 1) * limit;
 
   const subFields = await fieldModel.find({ parent: id, isDeleted: false });
