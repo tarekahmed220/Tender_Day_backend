dada25c3d228eb20a16cd4b8f6b40a0c43ef8981
tarekahmed220 <tarek.ahm12@gmail.com>
2025-05-07 03:07:16 +0300

    fix delete users

--- src/modules/utility/multer.js
+++ src/modules/utility/multer.js
@@ -14,7 +14,7 @@ const storage = multer.memoryStorage();
 
 export const upload = multer({
   storage,
-  limits: { fileSize: 5 * 1024 * 1024 },
+  limits: { fileSize: 30 * 1024 * 1024 },
   fileFilter: (req, file, cb) => {
     const allowedTypes = [
       "image/jpeg",
