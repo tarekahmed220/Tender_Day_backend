12191dc58490b48839c2648e3cf67291b1adb2be
tarekahmed220 <tarek.ahm12@gmail.com>
2025-05-08 19:02:29 +0300

    fix

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ const limiter = rateLimit({
   legacyHeaders: false,
 });
 
-app.use(express.json({ limit: "30mb" }));
+app.use(express.json({ limit: "30kb" }));
 app.use(express.urlencoded({ extended: true, limit: "30mb" }));
 
 app.use(cookieParser());
