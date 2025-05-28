27fb71085dc5c5981deb3ae34bbb76cc0416b964
tarekahmed220 <tarek.ahm12@gmail.com>
2025-05-29 02:32:06 +0300

    يس

--- src/modules/sitemap/sitemap.routes.js
+++ src/modules/sitemap/sitemap.routes.js
@@ -9,13 +9,19 @@ router.get("/sitemap.xml", async (req, res, next) => {
   try {
     const sitemapPath = path.resolve("public", "sitemap.xml");
 
+    // Generate sitemap if it doesn't exist
     if (!fs.existsSync(sitemapPath)) {
       await generateSitemap();
     }
 
-    res.setHeader("Content-Type", "application/xml");
-    res.sendFile(sitemapPath);
+    // Explicitly set Content-Type and charset
+    res.setHeader("Content-Type", "application/xml; charset=utf-8");
+
+    // Stream the file to ensure proper delivery
+    const stream = fs.createReadStream(sitemapPath);
+    stream.pipe(res);
   } catch (err) {
+    console.error("Sitemap error:", err);
     next(err);
   }
 });
