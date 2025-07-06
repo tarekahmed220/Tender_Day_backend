8184e2e8bb0720f0a880122d9e9770a45d630dd4
tarekahmed220 <tarek.ahm12@gmail.com>
2025-07-06 07:35:06 +0300

    Refactor SEO controller to remove createSeo method and add getSeoById method; update routes accordingly

--- src/modules/seo/seo.controller.js
+++ src/modules/seo/seo.controller.js
@@ -3,25 +3,6 @@ import catchError from "../../middleware/handleError.js";
 import AppError from "../utility/appError.js";
 import APIFeatures from "../utility/APIFeatures.js";
 
-export const createSeo = catchError(async (req, res, next) => {
-  const { id, name, title, description } = req.body;
-  const exists = await Seo.findOne({ id });
-  if (exists) return next(new AppError("هذه الصفحة موجودة بالفعل", 400));
-  const seo = await Seo.create({ id, name, title, description });
-  res.status(201).json({ message: "تمت الإضافة بنجاح", data: seo });
-});
-
-export const updateSeo = catchError(async (req, res, next) => {
-  const { id } = req.params;
-  const { title, description } = req.body;
-  const seo = await Seo.findOne({ id, deleted: false });
-  if (!seo) return next(new AppError("الصفحة غير موجودة", 404));
-  if (title) seo.title = title;
-  if (description) seo.description = description;
-  await seo.save();
-  res.status(200).json({ message: "تم التحديث بنجاح", data: seo });
-});
-
 export const getAllSeo = catchError(async (req, res, next) => {
   const features = new APIFeatures(Seo.find({ deleted: false }), req.query)
     .search()
@@ -37,4 +18,22 @@ export const getAllSeo = catchError(async (req, res, next) => {
   res
     .status(200)
     .json({ data, totalCount, pageCount: Math.ceil(totalCount / limit), skip });
+});
+
+export const getSeoById = catchError(async (req, res, next) => {
+  const { id } = req.params;
+  const seo = await Seo.findOne({ id, deleted: false });
+  if (!seo) return next(new AppError("الصفحة غير موجودة", 404));
+  res.status(200).json({ data: seo });
+});
+
+export const updateSeo = catchError(async (req, res, next) => {
+  const { id } = req.params;
+  const { title, description } = req.body;
+  const seo = await Seo.findOne({ id, deleted: false });
+  if (!seo) return next(new AppError("الصفحة غير موجودة", 404));
+  if (title) seo.title = title;
+  if (description) seo.description = description;
+  await seo.save();
+  res.status(200).json({ message: "تم التحديث بنجاح", data: seo });
 });
--- src/modules/seo/seo.routes.js
+++ src/modules/seo/seo.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { protect, restrictTo } from "../../middleware/authMiddleware.js";
-import { createSeo, updateSeo, getAllSeo } from "./seo.controller.js";
+import { updateSeo, getAllSeo, getSeoById } from "./seo.controller.js";
 import { validation } from "../../middleware/validation.js";
 import { seoValidation } from "./seo.validation.js";
 
 const router = express.Router();
 router.get("/", getAllSeo);
+router.get("/:id", getSeoById);
 
 router.put(
   "/:id",
